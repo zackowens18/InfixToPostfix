3f1db9078ab20b0c817b38518713980482d03bf4
zackowens18 <34559775+zackowens18@users.noreply.github.com>
2019-10-25 22:49:40 -0400

    Fixed some of the Errors with character reading

--- Infix to Postfix Python.py
+++ Infix to Postfix Python.py
@@ -1,10 +1,4 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[8]:
-
-
-functions = {'+':1, '-':1, '*':2, '/':2, '^':3}
+functions = {'+':1, '-':1, '*':2, '/':2, '^':3, '(':0 }
 formula = input("Formula to Evaluate:")
 formula = formula.replace(' ','')
 answer = ''
@@ -12,29 +6,21 @@ stack = []
 stringAdder = ''
 for i in range(len(formula)):
     if(formula[i] not in functions):
-        
-        if(formula[i]=='('):
-            stack.append('(')
+        if(formula[i]==')'):
+            top =''
+            while(top != '('):
+                answer+=top
+                top=stack.pop()
         else:
             stringAdder += formula[i]
     else:
-        stringAdder[::-1]
-        answer += stringAdder+','
-        stringAdder=''
-        if(len(stack)>0):
-            if(stack[-1]==')'):
-                while(stack.pop()!='('):
-                    answer +=stack.pop()
+        if(formula[i]=='('):
+            stack.append('(')
+        else:
+            while(len(stack)>0 and functions[formula[i]]>=functions[stack[-1]]):
+                answer+=stack.pop()
+            stack.append(formula[i])
         
-        stack.append(formula[i])
 
 print(answer)
         
-    
-
-
-# In[ ]:
-
-
-
-
